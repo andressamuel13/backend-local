f2a42d721673d9449d943d24aa3a7bda8ea8896d
brian <quinterobrian2403@gmail.com>
2025-05-12 20:21:51 -0500

    obtener todos los usuarios de la base locales

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.post("/register", (req, res) => {
   console.log(req.body);
   try {
     const id = UserRepository.create({ username, password });
-    res.send({ id });
+    res.send({ id }).json(({message : "user created"}));
   } catch (error) {
     res.status(400).send( error.message );
   }
@@ -25,6 +25,15 @@ app.post("/register", (req, res) => {
 
 app.post("/logout", (req, res) => {});
 app.get("/protected", (req, res) => {});
+
+app.get("/register", (req,res) => {
+  try{
+    const users = UserRepository.findAll();
+    res.json(users);
+  }catch (error) {
+    res.status(400).send(error.message);
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
--- userRepository.js
+++ userRepository.js
@@ -1,39 +1,60 @@
 import DBLocal from "db-local";
-const { Schema } = new DBLocal({path:'./db'})
-import crypto from 'crypto';
+const { Schema } = new DBLocal({ path: "./db" });
+import crypto from "crypto";
 
-const User = Schema('User',{
-    _id:{type:String , required:true},
-    username:{type:String , required:true},
-    password:{type:String , required:true},
-})
+const User = Schema("User", {
+  _id: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+});
 
 export class UserRepository {
   static create({ username, password }) {
-    if(typeof username !== 'string' ) {
-      throw new Error('username must be a string');
+    if (typeof username !== "string") {
+      throw new Error("username must be a string");
     }
 
-    if(typeof password !== 'string'){
-      throw new Error('password must be a string');
+    if (typeof password !== "string") {
+      throw new Error("password must be a string");
     }
 
-    if(username.length < 3 ) {
-      throw new Error('Username must be at least 3 characters long');
+    if (username.length < 3 && username.length > 0) {
+      throw new Error("Username must be at least 3 characters long");
     }
 
-    if(password.length < 3){
-      throw new Error('Password must be at least 3 characters long');
+    if (password.length < 3 && password.length > 0) {
+      throw new Error("Password must be at least 3 characters long");
+    }
+
+    if (username === "") {
+      throw new Error("the username cannot be empty");
+    }
+
+    if (password === "") {
+      throw new Error("the password cannot be empty");
+    }
+
+    if (username === "" && password === "") {
+      throw new Error("empty fields are not allowed");
     }
 
     const user = User.findOne({ username });
     if (user) {
-      throw new Error('User already exists');
+      throw new Error("User already exists");
     }
 
     const id = crypto.randomUUID();
 
-    User.create({ _id:id, username, password }).save()
+    User.create({ _id: id, username, password }).save();
   }
+
+  static findAll() {
+    const users = User.find();
+    if (users.length === 0) {
+      throw new Error("No users found");
+    }
+    return users;
+  }
+  
   static login({ username, password }) {}
 }
